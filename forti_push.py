10e1726f351710a2aea03b0b27cc0cb9a252c266
Martin Ohl <Martin.Ohl@ohl-net.eu>
2018-12-13 22:43:07 +0100

    Token Based Authentication and code cleanup

--- forti_push.py
+++ forti_push.py
@@ -1,252 +1,102 @@
 #!/usr/bin/env python
 import requests
-import argparse
 import sys
 import json
-from pprint import pprint
 
-fgt_ip = "ip address"
-user = "username"
-pw = "password"
+requests.packages.urllib3.disable_warnings()
 
-class FGT(object):
-    def __init__(self, host):
-        self.host = host
-        self.url_prefix = "https://" + self.host
 
-    def update_csrf(self):
-        # retrieve server csrf and update session's headers
-        for cookie in self.session.cookies:
-            if cookie.name == "ccsrftoken":
-                csrftoken = cookie.value[1:-1]  # token stored as a list
-                self.session.headers.update({"X-CSRFTOKEN": csrftoken})
+class Fortigate(object):
 
-    def login(self, name="admin", key="", csrf=True):
-        self.logout()
+    def __init__(self):
+        self.fgt = "1.1.1.1" #IP or Hostname FortiGate
+        self.token = "xxxxxxxxxxxxxxxxxx" #API token
 
-        self.session = requests.session()
-        url = self.url_prefix + "/logincheck"
-        try:
-            res = self.session.post(
-                url,
-                data="username=" + name + "&secretkey=" + key,
-                verify=False)
-            print "LOGIN successful"
-        except requests.exceptions.RequestException as e:
-            print(e)
-            print "LOGIN failed"
-            exit()
+        self.host = sys.argv[1]
+        self.group = "BadIPList" #Address Group Name
 
-        if res.text.find("error") != -1:
-            # found some error in the response, consider login failed
-            print "LOGIN failed"
-            return False
+        self.url = "https://" + self.fgt
+        self.headers = {"Authorization": "Bearer " + self.token}
+        self.verify = False
 
-        if csrf:
-            self.update_csrf()
-        return True
+    def get(self, type):
+        if type == 'address':
+            object = self.host
+        elif type == 'addrgrp':
+            object = self.group
 
-    def logout(self):
-        if hasattr(self, "session"):
-            url = self.url_prefix + "/logout"
-            self.session.post(url)
-            print "LOGOUT successful"
-
-    def get(self, url, **options):
-        url = self.url_prefix + url
-        try:
-            res = self.session.get(
-                url,
-                params=options.get("params"))
-        except requests.exceptions.RequestException as e:
-            print(e)
-            exit()
+        res = requests.get(self.url + '/api/v2/cmdb/firewall/' + type + "/" + object, headers=self.headers,
+                           verify=self.verify)
         return res
 
-    def post(self, url, override=None, **options):
-        url = self.url_prefix + url
-        data = options.get("data") if options.get("data") else None
-
-        # override session's HTTP method if needed
-        if override:
-            self.session.headers.update({"X-HTTP-Method-Override": override})
-        try:
-            res = self.session.post(
-                url,
-                params=options.get("params"),
-                data=json.dumps(data),
-                files=options.get("files"))
-        except requests.exceptions.RequestException as e:
-            print(e)
-            exit()
-
-        # restore original session
-        if override:
-            del self.session.headers["X-HTTP-Method-Override"]
+    def add_host(self):
+        data = {'name': self.host, 'type': "ipmask",'subnet': self.host + " 255.255.255.255"}
+        res = requests.post(self.url + '/api/v2/cmdb/firewall/address/', headers=self.headers, data=json.dumps(data),
+                            verify=self.verify)
         return res
 
-    def put(self, url, **options):
-        url = self.url_prefix + url
-        data = options.get("data") if options.get("data") else None
-        try:
-            res = self.session.put(
-                url,
-                params=options.get("params"),
-                data=json.dumps(data),
-                files=options.get("files"))
-        except requests.exceptions.RequestException as e:
-            print(e)
-            exit()
+    def add_group(self):
+        data = {'name': "BadIPList", 'member': [{'name': self.host}]}
+        res = requests.post(self.url + '/api/v2/cmdb/firewall/addrgrp/', headers=self.headers, data=json.dumps(data),
+                            verify=self.verify)
         return res
 
-
-def get_command(type, objname):
-    res = fgt.get(
-        url="/api/v2/cmdb/firewall/" + type + "/" + objname)
-    return res
-
-def create_host(host):
-    data = {'name': host, 'type': "ipmask",'subnet': host + " 255.255.255.255"}
-    res = fgt.post(
-        url="/api/v2/cmdb/firewall/address/",
-        data=data)
-
-def create_group(member):
-    data = {'name':"BadIPList", 'member': [{'name':member}]}
-    res = fgt.post(
-        url="/api/v2/cmdb/firewall/addrgrp/",
-        data=data)
-    return res
-
-def edit_group(members):
-    data = {'member': members}
-    res = fgt.put(
-        url="/api/v2/cmdb/firewall/addrgrp/BadIPList/",
-        data = data)
-    return res
-
-def get_json(response):
-    try:
-        rjson = response.json()
-    except UnicodeDecodeError as e:
-        print "Cannot decode json data in HTTP response"
-        return False
-    except:
-        e = sys.exc_info()[0]
-        #print(e)
-        return False
-    else:
-        return rjson
-
-def check_response(res):
-    rjson = get_json(res)
-    #pprint(rjson)
-    if not rjson:
-        #print "fail to retrieve JSON response"
-        pass
-    else:
-        status = rjson["http_status"]
-        if status == 200:
-            return status
-            #print "200 successful request"
-            pass
-        elif status == 400:
-            #print "400 Invalid request format"
-            pass
-        elif status == 403:
-            #print "403 Permission denied"
-            pass
-        elif status == 404:
-            #print "404 None existing resource"
-            pass
-        elif status == 405:
-            #print "405 Unsupported method"
-            pass
-        elif status == 424:
-            #print "424 Dependency error"
-            pass
-        elif status == 500:
-            #print "500 Internal server error"
-            pass
-        else:
-            #print status, "Unknown error"
-            pass
+    def edit_group(self, members):
+        data = {'member': members}
+        res = requests.put(self.url + '/api/v2/cmdb/firewall/addrgrp/'+ self.group + '/', headers=self.headers,
+                           data=json.dumps(data), verify=self.verify)
+        return res
 
 
 if __name__ == "__main__":
-    fgt = FGT(fgt_ip)
-    fgt.login(user,pw)
+    fgt = Fortigate()
+    check_host = fgt.get("address")
 
-    host = sys.argv[1]
+    if check_host.status_code != 200:
+        print("Address does not exist")
+        add_host = fgt.add_host()
+        if add_host.status_code != 200:
+            print("ERROR: Could not add host to FortiGate")
+            sys.exit(1)
+        print("Successfully added address")
+    elif check_host.status_code == 200:
+        print("Address exist already")
+    else:
+        print("Something went wrong.")
+        sys.exit(1)
 
-    # Check If Host already exist if not create new Host
-    res = get_command("address",host)
-    status = check_response(res)
-    if status == 200:
-        print "Host exist already"
+    check_group = fgt.get("addrgrp")
+    if check_group.status_code == 200:
+        print("Address Group exist already")
 
-        # Check If Group exist if not create new Group
-        res = get_command("addrgrp","BadIPList")
-        status = check_response(res)
-        if status == 200:
-            print "Group exist already"
+        try:
+            rjson = check_group.json()["results"]
+        except Exception as e:
+            print("Could not decode JSON data in HTTP response " + e)
+            sys.exit(1)
 
-            # Get the members of the group
-            res = get_command("addrgrp","BadIPList")
-            rjson = get_json(res)["results"]
-            for line in rjson:
-                members = line["member"]
-            for line in members:
-                if host in line["name"]:
-                    print "Host in group already"
-                    exit()
+        for line in rjson:
+            members = line["member"]
+        for line in members:
+            if fgt.host in line["name"]:
+                print("Host in group already")
+                sys.exit(1)
+            else:
+                print("Address is not member of this group")
+                members.append({'q_origin_key': fgt.host, 'name': fgt.host})
+                edit_group = fgt.edit_group(members)
+                if edit_group.status_code == 200:
+                    print("Address got added to this group")
+                    sys.exit(1)
 
-            print "Host is not in group"
-            members.append({'q_origin_key': host,'name': host})
-
-            res = edit_group(members)
-            status = check_response(res)
-            print "User got added to the group"
-        else:
-            print "Group does not exist"
-            res = create_group(host)
-            status = check_response(res)
-            print "Group got created"
+    elif check_group.status_code != 200:
+        print("Address Group does not exist")
+        create_group = fgt.add_group()
+        if create_group.status_code != 200:
+            print("ERROR: Could not add group to FortiGate")
+        print("Successfully created address group")
+        sys.exit(1)
 
     else:
-        # Create new host
-        print "Host does not exist"
-        res = create_host(host)
-        status = check_response(res)
-        print "New Host got created"
-
-        # Check if group exist if not create new group
-        res = get_command("addrgrp","BadIPList")
-        status = check_response(res)
-        if status == 200:
-            print "Group exist already"
-            # Get the members of the group
-            res = get_command("addrgrp","BadIPList")
-            rjson = get_json(res)["results"]
-            for line in rjson:
-                members = line["member"]
-            for line in members:
-                if host in line["name"]:
-                    print "Host in group already"
-                    fgt.logout()
-                    exit()
-
-            print "Host is not in group"
-            members.append({'q_origin_key': host,'name': host})
-
-            res = edit_group(members)
-            status = check_response(res)
-            print "Host got added to the group"
-
-        else:
-            print "Group does not exist"
-            res = create_group(host)
-            status = check_response(res)
-            print "Group got created"
-
-    fgt.logout()
+        print("Something went wrong.")
+        sys.exit(1)
